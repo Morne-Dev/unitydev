b4d6fe48db842e107f43c076eec29e2bf62c3f74
Morne-Dev <morne.dev114@gmail.com>
2022-04-18 23:42:20 +0200

    Fix: initial commit, safe

---
+++ Unity/ARCORONAGAZE1.1/Assets/Scripts/API.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class API : MonoBehaviour {
+
+    const string ENDPOINT = "https://api.covid19api.com/summary";
+
+    private void Start() {
+        GetTimeData();
+    }
+
+    public void GetTimeData() {
+        StartCoroutine(GetTimeDataRoutine());
+    }
+
+    IEnumerator GetTimeDataRoutine() {
+
+        UnityWebRequest request = UnityWebRequest.Get(ENDPOINT);
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError) {
+            Debug.Log("Network Error");
+        } else {
+            ParseData(request.downloadHandler.text);
+        }
+    }
+
+    void ParseData(string data) {
+        Debug.Log(data);
+    }
+}
